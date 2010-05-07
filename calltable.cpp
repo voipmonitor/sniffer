1f1e0b29ecff834162293c5bc2b3b77c5b703221
Martin Vit <martin@voipmonito.org>
2010-05-07 07:34:47 +0000

    flush data before closing to be sure (dont know if pcap close function will do this)

--- calltable.cpp
+++ calltable.cpp
@@ -347,6 +347,7 @@ Calltable::cleanup( time_t currtime ) {
 		// RTPTIMEOUT seconds of inactivity will save this call and remove from call table
 		if(currtime == 0 || (currtime - (*call)->get_last_packet_time() > RTPTIMEOUT)){
 			if ((*call)->get_f_pcap() != NULL){
+				pcap_dump_flush(call->get_f_pcap());
 				pcap_dump_close((*call)->get_f_pcap());
 				(*call)->set_f_pcap(NULL);
 			}
