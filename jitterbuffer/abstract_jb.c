5941145b5af4338bf695a7cd5b6372745ab39b8b
Martin Vit <martin@voipmonito.org>
2011-04-29 22:21:34 +0000

    do not write packet with f->data = 0x0, it casuses crashes

--- jitterbuffer/abstract_jb.c
+++ jitterbuffer/abstract_jb.c
@@ -396,7 +396,7 @@ static void jb_get_and_deliver(struct ast_channel *chan, struct timeval *mynow)
 		case JB_IMPL_OK:
 			/* deliver the frame */
 			//ast_write(chan, f);
-			if(chan->rawstream) {
+			if(chan->rawstream && f->data) {
 				//write frame to file
 				stmp = (short int)f->datalen;
 				if(chan->codec == PAYLOAD_SPEEX || chan->codec == PAYLOAD_G723 || chan->codec == PAYLOAD_G729) fwrite(&stmp, 1, sizeof(short int), chan->rawstream);   // write packet len
