e2edea9e3ba60f929fa6520c1260352f03e22dbe
Martin Vit <martin@voipmonito.org>
2012-10-03 14:13:40 +0000

    increase call-id len from 32 to 128 as it can be that high

--- calltable.h
+++ calltable.h
@@ -26,7 +26,7 @@
 
 #define MAX_IP_PER_CALL 30	//!< total maxumum of SDP sessions for one call-id
 #define MAX_SSRC_PER_CALL 30	//!< total maxumum of SDP sessions for one call-id
-#define MAX_CALL_ID 32		//!< max len of stored call-id
+#define MAX_CALL_ID 128		//!< max len of stored call-id
 #define MAX_FNAME 256		//!< max len of stored call-id
 #define MAX_RTPMAP 30          //!< max rtpmap records
 #define RTPTIMEOUT 300
