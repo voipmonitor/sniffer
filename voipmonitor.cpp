b48039ba70e5e0483d45d741e8c46217fdc88612
Martin Vit <martin@voipmonito.org>
2013-02-04 08:53:12 +0000

    change buffer size for filename to prevent crash in case pcap scandir is used

--- voipmonitor.cpp
+++ voipmonitor.cpp
@@ -1578,7 +1578,7 @@ int main(int argc, char *argv[]) {
 
 	if(opt_scanpcapdir[0] != '\0') {
 		// scan directory opt_scanpcapdir (typically /dev/shm/voipmonitor
-		char filename[32];
+		char filename[1024];
 		unsigned int tmp = 0;
 		unsigned int min = 0 - 1;
 		unsigned int max = 0;
