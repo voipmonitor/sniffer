9aa62c5e1f98d3a1e2de7e19b254b1faf201d882
Martin Vit <martin@voipmonito.org>
2013-03-07 11:10:54 +0000

    UserAgent in database and display was reverted caller was called and vice versa (probably since beginning..)

--- calltable.cpp
+++ calltable.cpp
@@ -325,7 +325,7 @@ Call::add_ip_port(in_addr_t addr, unsigned short port, char *ua, unsigned long u
 	}
 
 	if(ua_len && ua_len < 1024) {
-		char *tmp = iscaller ? this->b_ua : this->a_ua;
+		char *tmp = iscaller ? this->a_ua : this->b_ua;
 		memcpy(tmp, ua, MIN(ua_len, 1024));
 		tmp[MIN(ua_len, 1023)] = '\0';
 	}
