463fc952269d7e6295b4bb18a2f242528cef3cc2
rbucek <radek.bucek@dts-soft.cz>
2014-04-29 15:54:08 +0200

    use map<u_int64_t, list<sPacketTimeInfo>* > instead of map<u_int64_t, list<sPacketTimeInfo> > in packet ordering

--- pcap_queue.cpp
+++ pcap_queue.cpp
@@ -2943,7 +2943,7 @@ void *PcapQueue_readFromFifo::writeThreadFunction(void *arg, unsigned int arg2) 
 	}
 	pcap_block_store *blockStore;
 	map<pcap_block_store*, size_t> listBlockStore;
-	map<u_int64_t, list<sPacketTimeInfo> > listPacketTimeInfo;
+	map<u_int64_t, list<sPacketTimeInfo>* > listPacketTimeInfo;
 	while(!TERMINATING) {
 		if(DEBUG_SLEEP && access((this->pcapStoreQueue.fileStoreFolder + "/__/sleep").c_str(), F_OK ) != -1) {
 			sleep(1);
@@ -2972,16 +2972,23 @@ void *PcapQueue_readFromFifo::writeThreadFunction(void *arg, unsigned int arg2) 
 						pti.packet = (*blockStore)[i].packet;
 						pti.utime = pti.header->header_fix_size.ts_tv_sec * 1000000 + pti.header->header_fix_size.ts_tv_usec;
 						pti.at = at;
-						listPacketTimeInfo[pti.utime].push_back(pti);
+						map<u_int64_t, list<sPacketTimeInfo>* >::iterator iter = listPacketTimeInfo.find(pti.utime);
+						if(iter != listPacketTimeInfo.end()) {
+							iter->second->push_back(pti);
+						} else {
+							list<sPacketTimeInfo> *newList = new list<sPacketTimeInfo>;
+							newList->push_back(pti);
+							listPacketTimeInfo[pti.utime] = newList;
+						}
 					}
-					map<u_int64_t, list<sPacketTimeInfo> >::iterator first = listPacketTimeInfo.begin();
-					map<u_int64_t, list<sPacketTimeInfo> >::iterator last = listPacketTimeInfo.end();
+					map<u_int64_t, list<sPacketTimeInfo>* >::iterator first = listPacketTimeInfo.begin();
+					map<u_int64_t, list<sPacketTimeInfo>* >::iterator last = listPacketTimeInfo.end();
 					--last;
 					while(listPacketTimeInfo.size() && !TERMINATING) {
 						if(last->first - first->first > (unsigned)opt_pcap_queue_dequeu_window_length * 1000 && 
-						   at - first->second.begin()->at > (unsigned)opt_pcap_queue_dequeu_window_length * 1000) {
-							sPacketTimeInfo pti = *(first->second.begin());
-							first->second.pop_front();
+						   at - first->second->begin()->at > (unsigned)opt_pcap_queue_dequeu_window_length * 1000) {
+							sPacketTimeInfo pti = *(first->second->begin());
+							first->second->pop_front();
 							++sumPacketsCounterOut[0];
 							this->processPacket(pti.header, pti.packet, 
 									    pti.blockStore, pti.blockStoreIndex,
@@ -2992,7 +2999,8 @@ void *PcapQueue_readFromFifo::writeThreadFunction(void *arg, unsigned int arg2) 
 								this->blockStoreTrash_size += pti.blockStore->getUseSize();
 								listBlockStore.erase(pti.blockStore);
 							}
-							if(first->second.empty()) {
+							if(first->second->empty()) {
+								delete first->second;
 								listPacketTimeInfo.erase(first);
 								first = listPacketTimeInfo.begin();
 							}
@@ -3032,6 +3040,10 @@ void *PcapQueue_readFromFifo::writeThreadFunction(void *arg, unsigned int arg2) 
 	map<pcap_block_store*, size_t>::iterator iter;
 	for(iter = listBlockStore.begin(); iter != listBlockStore.end(); iter++) {
 		this->blockStoreTrash.push_back(iter->first);
+	}
+	while(listPacketTimeInfo.size()) {
+		delete listPacketTimeInfo.begin()->second;
+		listPacketTimeInfo.erase(listPacketTimeInfo.begin()->first);
 	}
 	this->writeThreadTerminated = true;
 	return(NULL);
