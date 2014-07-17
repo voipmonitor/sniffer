e4c3e3ee2131a47aab109aa56658684a292a6bca
rbucek <radek.bucek@dts-soft.cz>
2014-07-17 11:24:16 +0200

    suppress warning 'bad packet order' for verbosity <=1 can enable via manager command enable_bad_packet_order_warning

--- manager.cpp
+++ manager.cpp
@@ -64,6 +64,7 @@ extern char ssh_username[256];
 extern char ssh_password[256];
 extern char ssh_remote_listenhost[1024];
 extern unsigned int ssh_remote_listenport;
+extern int enable_bad_packet_order_warning;
 
 using namespace std;
 
@@ -1285,6 +1286,13 @@ getwav:
 	} else if(strstr(buf, "ac_remove_thread") != NULL) {
 		extern AsyncClose asyncClose;
 		asyncClose.removeThread();
+		if ((size = sendvm(client, sshchannel, "ok\n", 3, 0)) == -1){
+			cerr << "Error sending data to client" << endl;
+			return -1;
+		}
+		
+	} else if(strstr(buf, "enable_bad_packet_order_warning") != NULL) {
+		enable_bad_packet_order_warning = 1;
 		if ((size = sendvm(client, sshchannel, "ok\n", 3, 0)) == -1){
 			cerr << "Error sending data to client" << endl;
 			return -1;
