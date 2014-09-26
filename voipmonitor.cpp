463c26cee13db2ccc95ea9cddb5cd2e940b38843
“Petr <petr.halounek@voipmonitor.org>
2014-09-26 23:12:03 +0200

    No comment

--- voipmonitor.cpp
+++ voipmonitor.cpp
@@ -162,8 +162,8 @@ int opt_packetbuffered = 0;	// Make .pcap files writing ‘‘packet-buffered’
 				// more slow method, but you can use partitialy 
 				// writen file anytime, it will be consistent.
 	
-int opt_remotepartyid = 0;	//Rewrite caller? If sip invite contain header Remote-Party-ID, caller num/name is overwritten by its values.
 int opt_disableplc = 0 ;	// On or Off packet loss concealment			
+int opt_remotepartyid = 0;	//Rewrite caller? If sip invite contain header Remote-Party-ID, caller num/name is overwritten by its values.
 int opt_fork = 1;		// fork or run foreground 
 int opt_saveSIP = 0;		// save SIP packets to pcap file?
 int opt_saveRTP = 0;		// save RTP packets to pcap file?
