e648f66ea37a32df113346fef8023afae50cb419
rbucek <radek.bucek@dts-soft.cz>
2014-11-03 11:12:33 +0100

    use case insensitive for search items in cdr_[varchar] tables - fix via character set in declaration

--- sql_db.cpp
+++ sql_db.cpp
@@ -3078,7 +3078,7 @@ void SqlDb_mysql::createSchema(const char *host, const char *database, const cha
 	this->createFunction(
 			"BEGIN \
 				DECLARE _ID INT; \
-				SET _ID = (SELECT id FROM cdr_ua WHERE lower(ua) = lower(val)); \
+				SET _ID = (SELECT id FROM cdr_ua WHERE ua = val); \
 				IF ( _ID ) THEN \
 					RETURN _ID; \
 				ELSE  \
@@ -3086,12 +3086,12 @@ void SqlDb_mysql::createSchema(const char *host, const char *database, const cha
 					RETURN LAST_INSERT_ID(); \
 				END IF; \
 			END",
-			"getIdOrInsertUA", "(val VARCHAR(255)) RETURNS INT DETERMINISTIC");
+			"getIdOrInsertUA", "(val VARCHAR(255) CHARACTER SET latin1) RETURNS INT DETERMINISTIC");
 
 	this->createFunction(
 			"BEGIN \
 				DECLARE _ID INT; \
-				SET _ID = (SELECT id FROM cdr_sip_response WHERE lower(lastSIPresponse) = lower(val)); \
+				SET _ID = (SELECT id FROM cdr_sip_response WHERE lastSIPresponse = val); \
 				IF ( _ID ) THEN \
 					RETURN _ID; \
 				ELSE  \
@@ -3099,12 +3099,12 @@ void SqlDb_mysql::createSchema(const char *host, const char *database, const cha
 					RETURN LAST_INSERT_ID(); \
 				END IF; \
 			END",
-			"getIdOrInsertSIPRES", "(val VARCHAR(255)) RETURNS INT DETERMINISTIC");
+			"getIdOrInsertSIPRES", "(val VARCHAR(255)  CHARACTER SET latin1) RETURNS INT DETERMINISTIC");
 
 	this->createFunction(
 			"BEGIN \
 				DECLARE _ID INT; \
-				SET _ID = (SELECT id FROM contenttype WHERE lower(contenttype) = lower(val) LIMIT 1); \
+				SET _ID = (SELECT id FROM contenttype WHERE contenttype = val LIMIT 1); \
 				IF ( _ID ) THEN \
 					RETURN _ID; \
 				ELSE  \
@@ -3112,7 +3112,7 @@ void SqlDb_mysql::createSchema(const char *host, const char *database, const cha
 					RETURN LAST_INSERT_ID(); \
 				END IF; \
 			END",
-			"getIdOrInsertCONTENTTYPE", "(val VARCHAR(255)) RETURNS INT DETERMINISTIC");
+			"getIdOrInsertCONTENTTYPE", "(val VARCHAR(255) CHARACTER SET latin1) RETURNS INT DETERMINISTIC");
 
 	this->createProcedure(
 			"BEGIN \
