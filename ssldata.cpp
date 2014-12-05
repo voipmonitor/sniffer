0c55fa6d653f7c98ff60bb623614d28225e89cae
rbucek <radek.bucek@dts-soft.cz>
2014-12-05 16:28:41 +0100

    ssl - debugging

--- ssldata.cpp
+++ ssldata.cpp
@@ -63,102 +63,121 @@ void SslData::processData(u_int32_t ip_src, u_int32_t ip_dst,
 			}
 			cout << endl;
 		}
-		u_char *ssl_data;
-		u_int32_t ssl_datalen;
-		bool alloc_ssl_data = false;
-		if(this->remainData && this->remainDataLength) {
-			ssl_datalen = this->remainDataLength + dataItem->getDatalen();
-			ssl_data = new u_char[ssl_datalen];
-			memcpy(ssl_data, this->remainData, this->remainDataLength);
-			memcpy(ssl_data + this->remainDataLength, dataItem->getData(), dataItem->getDatalen());
-			delete [] this->remainData;
-			this->remainData = NULL;
-			this->remainDataLength = 0;
-			alloc_ssl_data = true;
-		} else {
-			ssl_data = dataItem->getData();
-			ssl_datalen = dataItem->getDatalen();
-		}
-		u_int32_t ssl_data_offset = 0;
-		while(ssl_data_offset < ssl_datalen &&
-		      ssl_datalen - ssl_data_offset >= 5) {
-			SslHeader header(ssl_data + ssl_data_offset, ssl_datalen - ssl_data_offset);
-			if(header.length && header.length + 5 <= ssl_datalen - ssl_data_offset) {
-				if(debugSave) {
-					cout << "SSL HEADER "
-					     << "content type: " << (int)header.content_type << " / "
-					     << "version: " << hex << header.version << dec << " / "
-					     << "length: " << header.length
-					     << endl;
-				}
-				u_int32_t _ip_src = dataItem->getDirection() == TcpReassemblyDataItem::DIRECTION_TO_DEST ? ip_src : ip_dst;
-				u_int32_t _ip_dst = dataItem->getDirection() == TcpReassemblyDataItem::DIRECTION_TO_DEST ? ip_dst : ip_src;
-				u_int16_t _port_src = dataItem->getDirection() == TcpReassemblyDataItem::DIRECTION_TO_DEST ? port_src : port_dst;
-				u_int16_t _port_dst = dataItem->getDirection() == TcpReassemblyDataItem::DIRECTION_TO_DEST ? port_dst : port_src;
-				vector<string> rslt_decrypt = decrypt_ssl((char*)(ssl_data + ssl_data_offset), header.length + 5, htonl(_ip_src), htonl(_ip_dst), _port_src, _port_dst);
-				for(size_t i = 0; i < rslt_decrypt.size(); i++) {
-					if(debugSave) {
-						cout << "DECRYPT DATA: " << rslt_decrypt[i] << endl;
-					}
-					if(!ethHeader || !ethHeaderLength) {
-						continue;
-					}
-					u_int32_t udpPacketLength = ethHeaderLength + sizeof(iphdr2) + sizeof(udphdr2) + rslt_decrypt[i].size();
-					u_char *udpPacket = new u_char[udpPacketLength];
-					memcpy(udpPacket, ethHeader, ethHeaderLength);
-					iphdr2 iphdr;
-					memset(&iphdr, 0, sizeof(iphdr2));
-					iphdr.version = 4;
-					iphdr.ihl = 5;
-					iphdr.protocol = IPPROTO_UDP;
-					iphdr.saddr = _ip_src;
-					iphdr.daddr = _ip_dst;
-					iphdr.tot_len = htons(sizeof(iphdr2) + sizeof(udphdr2) + rslt_decrypt[i].size());
-					iphdr.ttl = 50;
-					memcpy(udpPacket + ethHeaderLength, &iphdr, sizeof(iphdr2));
-					udphdr2 udphdr;
-					memset(&udphdr, 0, sizeof(udphdr2));
-					udphdr.source = htons(_port_src);
-					udphdr.dest = htons(_port_dst);
-					udphdr.len = htons(sizeof(udphdr2) + rslt_decrypt[i].size());
-					memcpy(udpPacket + ethHeaderLength + sizeof(iphdr2), &udphdr, sizeof(udphdr2));
-					memcpy(udpPacket + ethHeaderLength + sizeof(iphdr2) + sizeof(udphdr2), rslt_decrypt[i].c_str(), rslt_decrypt[i].size());
-					pcap_pkthdr header;
-					memset(&header, 0, sizeof(pcap_pkthdr));
-					header.ts.tv_sec = dataItem->getTime().tv_sec;
-					header.ts.tv_usec = dataItem->getTime().tv_usec;
-					header.caplen = udpPacketLength;
-					header.len = udpPacketLength;
-					int was_rtp = 0;
-					int voippacket = 0;
-					if(preProcessPacket) {
-						preProcessPacket->push(0, _ip_src, _port_src, _ip_dst, _port_dst, 
-								       (char*)(udpPacket + ethHeaderLength + sizeof(iphdr2) + sizeof(udphdr2)), rslt_decrypt[i].size(), ethHeaderLength + sizeof(iphdr2) + sizeof(udphdr2),
-								       handle, &header, udpPacket, true, 
-								       false, (iphdr2*)(udpPacket + ethHeaderLength), 1,
-								       NULL, 0, dlt, sensor_id);
-					} else {
-						process_packet(0, _ip_src, _port_src, _ip_dst, _port_dst, 
-							       (char*)rslt_decrypt[i].c_str(), rslt_decrypt[i].size(), ethHeaderLength + sizeof(iphdr2) + sizeof(udphdr2),
-							       handle, &header, udpPacket, 
-							       false, &was_rtp, (iphdr2*)(udpPacket + ethHeaderLength), &voippacket, 1,
-							       NULL, 0, dlt, sensor_id);
-						delete [] udpPacket;
-					}
-				}
-				ssl_data_offset += header.length + 5;
+		for(int pass = 0; pass < 2; pass++) {
+			u_char *ssl_data;
+			u_int32_t ssl_datalen;
+			bool alloc_ssl_data = false;
+			if(this->remainData) {
+				ssl_datalen = this->remainDataLength + dataItem->getDatalen();
+				ssl_data = new u_char[ssl_datalen];
+				memcpy(ssl_data, this->remainData, this->remainDataLength);
+				memcpy(ssl_data + this->remainDataLength, dataItem->getData(), dataItem->getDatalen());
+				delete [] this->remainData;
+				this->remainData = NULL;
+				alloc_ssl_data = true;
 			} else {
-				break;
+				ssl_data = dataItem->getData();
+				ssl_datalen = dataItem->getDatalen();
 			}
-		}
-		if(ssl_data_offset < ssl_datalen) {
-			this->remainDataLength = ssl_datalen - ssl_data_offset;
-			this->remainData =  new u_char[this->remainDataLength];
-			memcpy(this->remainData, ssl_data + ssl_data_offset, this->remainDataLength);
-			cout << "REMAIN DATA LENGTH: " << ssl_datalen - ssl_data_offset << endl;
-		}
-		if(alloc_ssl_data) {
-			delete [] ssl_data;
+			u_int32_t ssl_data_offset = 0;
+			while(ssl_data_offset < ssl_datalen &&
+			      ssl_datalen - ssl_data_offset >= 5) {
+				SslHeader header(ssl_data + ssl_data_offset, ssl_datalen - ssl_data_offset);
+				if(header.length && header.length + 5 <= ssl_datalen - ssl_data_offset) {
+					if(debugSave) {
+						cout << "SSL HEADER "
+						     << "content type: " << (int)header.content_type << " / "
+						     << "version: " << hex << header.version << dec << " / "
+						     << "length: " << header.length
+						     << endl;
+					}
+					u_int32_t _ip_src = dataItem->getDirection() == TcpReassemblyDataItem::DIRECTION_TO_DEST ? ip_src : ip_dst;
+					u_int32_t _ip_dst = dataItem->getDirection() == TcpReassemblyDataItem::DIRECTION_TO_DEST ? ip_dst : ip_src;
+					u_int16_t _port_src = dataItem->getDirection() == TcpReassemblyDataItem::DIRECTION_TO_DEST ? port_src : port_dst;
+					u_int16_t _port_dst = dataItem->getDirection() == TcpReassemblyDataItem::DIRECTION_TO_DEST ? port_dst : port_src;
+					vector<string> rslt_decrypt = decrypt_ssl((char*)(ssl_data + ssl_data_offset), header.length + 5, htonl(_ip_src), htonl(_ip_dst), _port_src, _port_dst);
+					for(size_t i = 0; i < rslt_decrypt.size(); i++) {
+						if(debugSave) {
+							cout << "DECRYPT DATA: " << rslt_decrypt[i] << endl;
+						}
+						if(!ethHeader || !ethHeaderLength) {
+							continue;
+						}
+						u_int32_t udpPacketLength = ethHeaderLength + sizeof(iphdr2) + sizeof(udphdr2) + rslt_decrypt[i].size();
+						u_char *udpPacket = new u_char[udpPacketLength];
+						memcpy(udpPacket, ethHeader, ethHeaderLength);
+						iphdr2 iphdr;
+						memset(&iphdr, 0, sizeof(iphdr2));
+						iphdr.version = 4;
+						iphdr.ihl = 5;
+						iphdr.protocol = IPPROTO_UDP;
+						iphdr.saddr = _ip_src;
+						iphdr.daddr = _ip_dst;
+						iphdr.tot_len = htons(sizeof(iphdr2) + sizeof(udphdr2) + rslt_decrypt[i].size());
+						iphdr.ttl = 50;
+						memcpy(udpPacket + ethHeaderLength, &iphdr, sizeof(iphdr2));
+						udphdr2 udphdr;
+						memset(&udphdr, 0, sizeof(udphdr2));
+						udphdr.source = htons(_port_src);
+						udphdr.dest = htons(_port_dst);
+						udphdr.len = htons(sizeof(udphdr2) + rslt_decrypt[i].size());
+						memcpy(udpPacket + ethHeaderLength + sizeof(iphdr2), &udphdr, sizeof(udphdr2));
+						memcpy(udpPacket + ethHeaderLength + sizeof(iphdr2) + sizeof(udphdr2), rslt_decrypt[i].c_str(), rslt_decrypt[i].size());
+						pcap_pkthdr header;
+						memset(&header, 0, sizeof(pcap_pkthdr));
+						header.ts.tv_sec = dataItem->getTime().tv_sec;
+						header.ts.tv_usec = dataItem->getTime().tv_usec;
+						header.caplen = udpPacketLength;
+						header.len = udpPacketLength;
+						int was_rtp = 0;
+						int voippacket = 0;
+						if(preProcessPacket) {
+							preProcessPacket->push(0, _ip_src, _port_src, _ip_dst, _port_dst, 
+									       (char*)(udpPacket + ethHeaderLength + sizeof(iphdr2) + sizeof(udphdr2)), rslt_decrypt[i].size(), ethHeaderLength + sizeof(iphdr2) + sizeof(udphdr2),
+									       handle, &header, udpPacket, true, 
+									       false, (iphdr2*)(udpPacket + ethHeaderLength), 1,
+									       NULL, 0, dlt, sensor_id);
+						} else {
+							process_packet(0, _ip_src, _port_src, _ip_dst, _port_dst, 
+								       (char*)rslt_decrypt[i].c_str(), rslt_decrypt[i].size(), ethHeaderLength + sizeof(iphdr2) + sizeof(udphdr2),
+								       handle, &header, udpPacket, 
+								       false, &was_rtp, (iphdr2*)(udpPacket + ethHeaderLength), &voippacket, 1,
+								       NULL, 0, dlt, sensor_id);
+							delete [] udpPacket;
+						}
+					}
+					ssl_data_offset += header.length + 5;
+				} else {
+					break;
+				}
+			}
+			if(pass == 0) {
+				bool ok = false;
+				if(this->remainDataLength &&
+				   !ssl_data_offset &&
+				   _checkOkSslData(dataItem->getData(), dataItem->getDatalen())) {
+					// next pass with ignore remainData
+					if(debugSave) {
+						cout << "SKIP REMAIN DATA" << endl;
+					}
+				} else {
+					if(ssl_data_offset < ssl_datalen) {
+						this->remainDataLength = ssl_datalen - ssl_data_offset;
+						this->remainData =  new u_char[this->remainDataLength];
+						memcpy(this->remainData, ssl_data + ssl_data_offset, this->remainDataLength);
+						if(debugSave) {
+							cout << "REMAIN DATA LENGTH: " << ssl_datalen - ssl_data_offset << endl;
+						}
+					}
+					ok = true;
+				}
+				if(alloc_ssl_data) {
+					delete [] ssl_data;
+				}
+				if(ok) {
+					break;
+				}
+			}
 		}
 	}
 	delete data;
