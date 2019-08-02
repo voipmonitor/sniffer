8346e8fc9efdf3b38820b6260c605fb4ca760fe0
rbucek <radek.bucek@dts-soft.cz>
2019-08-02 13:45:48 +0200

    tikcet VS-941 use ssrc in publish always as hexa

--- sniff.cpp
+++ sniff.cpp
@@ -4860,9 +4860,7 @@ inline void process_packet__parse_rtcpxr(Call* call, packet_s_process *packetS, 
 	if(ssrc.length() > 2 && ssrc[0] == '0' && ssrc[1] == 'x') {
 		sscanf(ssrc.c_str() + 2, "%x", &ssrc_int);
 	} else {
-		ssrc_int = string_is_numeric(ssrc.c_str()) ?
-			    atoll(ssrc.c_str()) :
-			    strtol(ssrc.c_str(), 0, 16);
+		ssrc_int = strtol(ssrc.c_str(), 0, 16);
 	}
 	call->rtcpXrData.add(ssrc_int, tv, moslq, nlr, ipLocal, ipRemote);
 }
