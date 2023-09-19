05a898aba5a4e6c62ab4101f600d15c84c024cbd
Martin Vit <martin@voipmonitor.org>
2023-09-20 00:00:22 +0200

    add noop instruction in the spin fast spinlock

--- sync.h
+++ sync.h
@@ -2,7 +2,7 @@
 #define SYNC_H
 
 
-#define __SYNC_LOCK(vint) while(__sync_lock_test_and_set(&vint, 1)) {};
+#define __SYNC_LOCK(vint) while(__sync_lock_test_and_set(&vint, 1)) {__asm__ volatile ("pause");};
 #define __SYNC_LOCK_USLEEP(vint, us_sleep) while(__sync_lock_test_and_set(&vint, 1)) { if(us_sleep) { USLEEP(us_sleep); } }
 #define __SYNC_UNLOCK(vint) __sync_lock_release(&vint);
 
