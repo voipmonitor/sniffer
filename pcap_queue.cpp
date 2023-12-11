7d56393a91384ad896989cda1dcfb8c5ebb47feb
rbucek <radek.bucek@dts-soft.cz>
2023-12-11 11:08:00 +0100

    ticket VS-1524 better filter for server/client traffic in packtebuffer_sender mode

--- pcap_queue.cpp
+++ pcap_queue.cpp
@@ -4110,10 +4110,12 @@ bool PcapQueue_readFromInterface_base::check_filter_ip(pcap_pkthdr* header, u_ch
 	extern bool opt_is_client_packetbuffer_sender;
 	if(opt_is_client_packetbuffer_sender) {
 		iphdr2 *iphdr = (iphdr2*)(packet + checkProtocolData->header_ip_offset);
-		if(iphdr->get_daddr() == snifferClientOptions.host_ip &&
-		   iphdr->get_protocol(header->caplen - checkProtocolData->header_ip_offset) == IPPROTO_TCP) {
+		if(iphdr->get_protocol(header->caplen - checkProtocolData->header_ip_offset) == IPPROTO_TCP) {
 			tcphdr2 *header_tcp = (tcphdr2*)((char*)iphdr + iphdr->get_hdr_size());
-			if((unsigned)header_tcp->get_dest() == snifferClientOptions.port) {
+			if((iphdr->get_daddr() == snifferClientOptions.host_ip &&
+			    (unsigned)header_tcp->get_dest() == snifferClientOptions.port) ||
+			   (iphdr->get_saddr() == snifferClientOptions.host_ip &&
+			    (unsigned)header_tcp->get_source() == snifferClientOptions.port)) {
 				return(false);
 			}
 		}
