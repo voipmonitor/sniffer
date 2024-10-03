3b776fc2b55263d6fed95aed79e9dc43f921889b
rbucek <radek.bucek@dts-soft.cz>
2024-10-03 14:01:53 +0200

    commit omitted change

--- tests_utils.cpp
+++ tests_utils.cpp
@@ -1469,7 +1469,7 @@ void test() {
 		call->type_base = INVITE;
 		call->force_spool_path = opt_test_arg;
 		sverb.noaudiounlink = true;
-		call->convertRawToWav(NULL);
+		call->convertRawToWav(NULL, 0);
 		}
 		break;
 	case _param_find_country_for_number:
